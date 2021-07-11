56f4548d27eaa1a244c1c98339a6cbfeb4dd896c
Maksym Revutskyi <mrevutskyi@linkedin.com>
2021-07-11 11:44:11 -0700

    Add Tox config to test agains multiple versions of SQLAlchemy and Flask

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,23 +7,22 @@ from .helpers import validate_schema
 
 
 class BaseTestClass:
-    """Tests corresponding to the `Fetching Data`_ section of the JSON API specification.
-
-    .. _Fetching Data: https://jsonapi.org/format/#fetching
-
-    """
+    """Base test class that contains required fixtures."""
 
     @classmethod
     def setup_class(cls):
         cls.engine = create_engine('sqlite://')
-        scoped_session_cls = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=cls.engine))
-        cls.session = scoped_session_cls()
+        cls.scoped_session_cls = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=cls.engine))
 
     def setup_method(self):
         app = Flask(__name__)
         app.config['TESTING'] = True
         self.app = app
         self.client = app.test_client()
+        self.session = self.scoped_session_cls()
+
+    def teardown_method(self):
+        self.session.close()
 
     def fetch_and_validate(self, uri: str, expected_response_code: int = 200):
         response = self.client.get(uri)
